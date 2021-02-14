dcbde9d6f2fa227ef491c0ed21aba788acc97cf0
Yufan You <ouuansteve@gmail.com>
2021-02-14 11:11:24 +0800

    No endl

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import sys
 username = sys.argv[1]
 
 res = requests.get(f'https://api.github.com/users/{username}').json()
-print(f'{res["id"]}+{res["login"]}@users.noreply.github.com')
+print(f'{res["id"]}+{res["login"]}@users.noreply.github.com', end='')
